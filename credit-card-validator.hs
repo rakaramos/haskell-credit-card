0cf33b71cb7f77c408e398a323780f0634c53000
rakaramos <raka.cdc@gmail.com>
2017-01-31 17:34:05 -0200

    Building validations

--- credit-card-validator.hs
+++ credit-card-validator.hs
@@ -4,11 +4,54 @@
 -- Diners Club bin list: Card numbers begin with 300 through 305, 36 or 38. 
 -- Discover bin list: Card numbers begin with 6011 or 65.
 -- JCB bin list: Card numbers begin with 35.
+import Data.Char
 
-data CreditCardIssuer = Visa | Mastercard | Discover | Amex | Diners | JCB deriving (Show)
-data CreditCardError = UnknownIssuer | InvalidNumber | Expired deriving (Show)
-data MajorIndustryIdentifier = Airlines | Travel | Banking | Petroleum | Telecom | NationalAssigment deriving (Show)
+data CreditCardIssuer =
+      Visa 
+    | Mastercard 
+    | Discover 
+    | Amex 
+    | Diners 
+    | JCB deriving (Show)
 
-lastButOne xs = xs!!((length xs) - 2)
+data CreditCardError =
+      UnknownIssuer 
+    | InvalidNumber
+    | Expired deriving (Show)
 
-slice xs = if (length xs) == 0 then xs else slice (head (tail xs)):xs
+data MajorIndustryIdentifier = 
+      Airlines 
+    | Travel
+    | Banking 
+    | Petroleum 
+    | Telecom 
+    | NationalAssigment deriving (Show)
+
+doubleEveryOther (a,b) 
+    | a `mod` 2 == 0 = (a,sumIfNeeded (2*b))
+    | otherwise  = (a,b)
+
+sumIfNeeded a
+    | a >= 10 = a-9
+    | otherwise = a
+
+chunkIndexed = reverse . snd . unzip . (map doubleEveryOther) . (zip [1..]) . reverse
+checksum xs = foldl (+) 0 $ chunkIndexed xs
+luhn xs = mod (checksum xs) 10 == 0
+
+cardIssuer :: Integral a => [a] -> Maybe CreditCardIssuer
+cardIssuer []    = Nothing
+cardIssuer (4:_) = Just Visa
+cardIssuer (5:x:_) | x `elem` [1..5] = Just Mastercard
+cardIssuer (3:x:_) | x `elem` [4,7]  = Just Amex
+cardIssuer (3:x:_) | x `elem` [6,8]  = Just Diners
+cardIssuer (6:0:1:1:_) = Just Discover
+cardIssuer (6:5:_) = Just Discover
+cardIssuer xs = Nothing
+
+cardIssuer' = cardIssuer . (map digitToInt)
+
+--validCreditCard :: [a] -> Either CreditCardError (CreditCardIssuer,MajorIndustryIdentifier)
+--validCreditCard xs
+--    | luhn xs = 
+--    | otherwise = Left InvalidNumber
