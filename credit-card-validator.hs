f030bfea72e1e24bce64eb6439c97aab6b30ff4e
Luca Manique <lucamanique@Luca-Manique.local>
2017-02-02 16:34:32 -0200

    Adding function signatures and polishing the code

--- credit-card-validator.hs
+++ credit-card-validator.hs
@@ -28,43 +28,48 @@ data MajorIndustryIdentifier =
     | Telecom 
     | NationalAssigment deriving (Show)
 
+-- Type Synonyms
+type CreditCardNumber = String
+type CreditCardPrefix = String
+type CreditCardDigit = Char
+
+
+doubleEveryOther :: (Integral a, Integral b) => (a,b) -> b
 doubleEveryOther (index,value) 
     | even index = sumDigits (2 * value)
     | otherwise  = value
 
+sumDigits :: Integral a => a -> a
 sumDigits 0 = 0
 sumDigits x
     | modulo == 0 = 9
     | otherwise   = modulo
     where  modulo = x `mod` 9
 
-chunkIndexed = reverse . (map doubleEveryOther) . (zip [1..]) . reverse
-checksum = sum . chunkIndexed
-luhn xs = (checksum xs) `mod` 10 == 0
+getChecksum :: Integral a => [a] -> a
+getChecksum = sum . reverse . (map doubleEveryOther) . (zip [1..]) . reverse
 
 
-type CreditCardNumber = String
-type Prefix = String
-type PrefixDigit = Char
+validateCreditCardNumber :: CreditCardNumber -> Bool
+validateCreditCardNumber cc = ((getChecksum . (map digitToInt)) cc) `mod` 10 == 0
+
+
 -- Concat prefix with each prefix digit
-(+|+) :: Prefix -> [PrefixDigit] -> [Prefix]
+(+|+) :: CreditCardPrefix -> [CreditCardDigit] -> [CreditCardPrefix]
 (+|+) prefix digits = map ((prefix ++) . (:[])) digits
 --(+|+) prefix digits = map (\d ->  prefix ++ [d]) digits
 
-hasAnyPrefixIn :: CreditCardNumber -> [Prefix] -> Bool
+hasAnyPrefixIn :: CreditCardNumber -> [CreditCardPrefix] -> Bool
 hasAnyPrefixIn cc prefixes = any (`isPrefixOf` cc) prefixes
 
-cardIssuer :: CreditCardNumber -> Maybe CreditCardIssuer
-cardIssuer cc
+getCardIssuer :: CreditCardNumber -> Maybe CreditCardIssuer
+getCardIssuer cc
     | cc `hasAnyPrefixIn` ["4"]                = Just Visa
     | cc `hasAnyPrefixIn` ("5" +|+ ['1'..'5']) = Just Mastercard
     | cc `hasAnyPrefixIn` ("3" +|+ ['4', '7']) = Just Amex
     | cc `hasAnyPrefixIn` ("3" +|+ ['6', '8']) = Just Diners
     | cc `hasAnyPrefixIn` ["6011", "65"]       = Just Discover
     | otherwise                                = Nothing
-
---cardIssuer' :: String -> Maybe CreditCardIssuer
---cardIssuer' = cardIssuer . (map digitToInt)
 
 --validCreditCard :: [a] -> Either CreditCardError (CreditCardIssuer,MajorIndustryIdentifier)
 --validCreditCard xs
